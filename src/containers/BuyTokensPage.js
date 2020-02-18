fd24c49534efb17c98964aa64d07c4cb28e0f776
Lucas <lucas.huet-de-guerville@epitech.eu>
2020-02-18 16:37:04 +0100

    change nbrs of tokens in buy token page to fit with the main page

--- src/containers/BuyTokensPage.js
+++ src/containers/BuyTokensPage.js
@@ -18,13 +18,13 @@ const DashboardPage = () => {
 
         <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
           <img src={"../images/heetch.jpg"} style={{width:"200px", height: "200px"}}/>
-          <p>Tokens Restant: 42</p>
+          <p>Tokens Restant: 10</p>
           <button className="button">Acheter Tokens</button>
         </div>
 
         <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 m-b-15 ">
           <img src={"../images/kolett.png"} style={{width:"200px", height: "auto"}}/>
-          <p>Tokens Restant: 23</p>
+          <p>Tokens Restant: 53</p>
           <button className="button"> Acheter Tokens</button>
         </div>
       </div>
